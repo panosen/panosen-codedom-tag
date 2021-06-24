98a276193a1884bcf27ece423bf6814346f3e6a8
hc_zhang <hc_zhang@trip.com>
2021-06-24 19:33:37 +0800

    update

---
+++ Panosen.CodeDom.Tag.Core.MSTest/UnitTest4.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Panosen.CodeDom.Tag.Basic;
+using Panosen.CodeDom.Tag.Basic.Engine;
+using Panosen.CodeDom.Tag.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panosen.CodeDom.Tag.Core.MSTest
+{
+    [TestClass]
+    public class UnitTest4
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var component = PrepareComponent();
+
+            var builder = new StringBuilder();
+
+            var options = new GenerateOptions()
+                .AddComponentEngine<BasicComponent>(new BasicComponentEngine());
+
+            new TagEngineCore().Generate(component, builder, options);
+
+            var actual = builder.ToString();
+
+            var expected = PrepareExpected();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        private string PrepareExpected()
+        {
+            return @"<div>aaa</div>
+<div>aaa</div>
+<div>aaa</div>";
+        }
+
+        private List<Component> PrepareComponent()
+        {
+            List<Component> components = new List<Component>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                var component = new BasicComponent();
+                component.SetContent("aaa");
+                components.Add(component);
+            }
+
+            return components;
+        }
+    }
+}
--- Panosen.CodeDom.Tag.Core/TagEngineCore.cs
+++ Panosen.CodeDom.Tag.Core/TagEngineCore.cs
@@ -12,6 +12,12 @@ namespace Panosen.CodeDom.Tag.Core
     /// </summary>
     public class TagEngineCore
     {
+        /// <summary>
+        /// Generate
+        /// </summary>
+        /// <param name="component"></param>
+        /// <param name="codeWriter"></param>
+        /// <param name="options"></param>
         public void Generate(Component component, CodeWriter codeWriter, GenerateOptions options = null)
         {
             options = options ?? new GenerateOptions();
@@ -23,6 +29,38 @@ namespace Panosen.CodeDom.Tag.Core
             }
 
             engine.Generate(component, codeWriter, options);
+        }
+
+        /// <summary>
+        /// Generate
+        /// </summary>
+        /// <param name="components"></param>
+        /// <param name="codeWriter"></param>
+        /// <param name="options"></param>
+        public void Generate(List<Component> components, CodeWriter codeWriter, GenerateOptions options = null)
+        {
+            if (components == null || components.Count == 0)
+            {
+                return;
+            }
+            options = options ?? new GenerateOptions();
+
+            for (int i = 0; i < components.Count; i++)
+            {
+                var component = components[i];
+
+                var engine = options.GetComponentEngine(component);
+                if (engine == null)
+                {
+                    continue;
+                }
+
+                engine.Generate(component, codeWriter, options);
+                if (i < components.Count - 1)
+                {
+                    codeWriter.WriteLine();
+                }
+            }
         }
     }
 }
