463c96957c29d41949b003971a8cf7e212b0f5f5
hc_zhang <hc_zhang@trip.com>
2021-06-24 19:38:36 +0800

    update bat

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,12 +4,13 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
-move /Y Panosen.CodeDom.Tag\bin\Release\Panosen.CodeDom.Tag.*.nupkg D:\LocalSavoryNuget\
-move /Y Panosen.CodeDom.Tag.Core\bin\Release\Panosen.CodeDom.Tag.Core.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Tag.Engine\bin\Release\Panosen.CodeDom.Tag.Engine.*.nupkg D:\LocalSavoryNuget\
-move /Y Panosen.CodeDom.Tag.Basic\bin\Release\Panosen.CodeDom.Tag.Basic.*.nupkg D:\LocalSavoryNuget\
-move /Y Panosen.CodeDom.Tag.Basic.Engine\bin\Release\Panosen.CodeDom.Tag.Basic.Engine.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.Tag.Html\bin\Release\Panosen.CodeDom.Tag.Html.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Tag.Script\bin\Release\Panosen.CodeDom.Tag.Script.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Tag.Script.Engine\bin\Release\Panosen.CodeDom.Tag.Script.Engine.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.Tag\bin\Release\Panosen.CodeDom.Tag.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.Tag.Basic\bin\Release\Panosen.CodeDom.Tag.Basic.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.Tag.Basic.Engine\bin\Release\Panosen.CodeDom.Tag.Basic.Engine.*.nupkg D:\LocalSavoryNuget\
+move /Y Panosen.CodeDom.Tag.Core\bin\Release\Panosen.CodeDom.Tag.Core.*.nupkg D:\LocalSavoryNuget\
 
 pause
