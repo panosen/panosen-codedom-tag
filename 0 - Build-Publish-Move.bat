8fdf074175db3e49aad969af5ece7709a569e224
hc_zhang <hc_zhang@trip.com>
2021-07-21 15:24:34 +0800

    remove Panosen.CodeDom.Tag.Basic.csproj

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -9,8 +9,6 @@ move /Y Panosen.CodeDom.Tag.Html\bin\Release\Panosen.CodeDom.Tag.Html.*.nupkg D:
 move /Y Panosen.CodeDom.Tag.Script\bin\Release\Panosen.CodeDom.Tag.Script.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Tag.Script.Engine\bin\Release\Panosen.CodeDom.Tag.Script.Engine.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Tag\bin\Release\Panosen.CodeDom.Tag.*.nupkg D:\LocalSavoryNuget\
-move /Y Panosen.CodeDom.Tag.Basic\bin\Release\Panosen.CodeDom.Tag.Basic.*.nupkg D:\LocalSavoryNuget\
-move /Y Panosen.CodeDom.Tag.Basic.Engine\bin\Release\Panosen.CodeDom.Tag.Basic.Engine.*.nupkg D:\LocalSavoryNuget\
 move /Y Panosen.CodeDom.Tag.Core\bin\Release\Panosen.CodeDom.Tag.Core.*.nupkg D:\LocalSavoryNuget\
 
 pause
